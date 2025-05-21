48a883096bd93c1fa19588f328ba2a26b2910278
Matthew Ho <ssoohoya@gmail.com>
2025-05-21 14:35:06 -0700

    Added 2 junit test(LoginInPageTests and SignUpPageTests)

---
+++ Tests/SignUpPageTests.java
@@ -0,0 +1,26 @@
+import com.example.cs210finalproject.User;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+// author: Matthew Ho
+public class SignUpPageTests {
+
+    private final String testUsername = "testUser";
+    private final String testPassword = "Valid!!123";
+
+
+    @AfterEach
+    void tearDown() {
+        User.userDatabase.remove(testUsername);
+    }
+
+    @Test
+    void testSignUpCreatesAccount() {
+        boolean success = User.signUp(testUsername, testPassword);
+
+        assertTrue(success, "Account should be created with valid username and password.");
+        assertNotNull(User.userDatabase.get(testUsername), "User database should contain the new account.");
+    }
+
+}
